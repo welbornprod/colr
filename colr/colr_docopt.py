85a39bfba7ea74724ff3488c550167cadd3f17ca
Christopher Welborn <cj@welbornprod.com>
2017-03-14 23:38:57 -0500

    Whitespace changes.
    There was a reason for this change, but it's not important.

--- colr/colr_docopt.py
+++ colr/colr_docopt.py
@@ -4,9 +4,9 @@
 """
 import sys
 
-from .colr import Colr as C
-
 import docopt
+
+from .colr import Colr as C
 
 # This can be set with a call to colr_docopt.docopt().
 # When set, the script name can be colorized.
@@ -109,20 +109,26 @@ def docopt(
     """
 This is a wrapper for docopt.docopt that also sets SCRIPT to `script`.
     When SCRIPT is set, it can be colorized for the usage string.
-    A dict of Colr options can be passed with `colors` to alter the styles.
+    A dict of Colr options can be passed with `colors` to alter the
+    styles.
     Available color options keys:
         desc     : Colr args for the description of options.
         label    : Colr args for the 'Usage:' and 'Options:' labels.
-        header   : Colr args for the top line (program name), and any other
-                   line that is not indented at all.
+        header   : Colr args for the top line (program name), and any
+                   other line that is not indented at all.
         options  : Colr args for the options themselves ('-h,--help').
-        script   : Colr args for the script name (if found in the usage text).
+        script   : Colr args for the script name, if found in the usage
+                   text.
         version  : Colr args for the version when --version is used.
 
     Example:
-        # `colors` only updates the default settings. You must override them
-        # to change ALL the settings.
-        argd = docopt(..., script=SCRIPT, colors={'script': {'fore': 'red'}})
+        # `colors` only updates the default settings. You must override
+        # them to change ALL the settings.
+        argd = docopt(
+            ...,
+            script=SCRIPT,
+            colors={'script': {'fore': 'red'}}
+        )
 
 Original docopt documentation follows:
     """
@@ -133,7 +139,9 @@ Original docopt documentation follows:
     SCRIPT = script
     if colors:
         # Setup colors, if any were given.
-        ARGS_DESC.update(colors.get('desc', colors.get('description', {})))
+        ARGS_DESC.update(
+            colors.get('desc', colors.get('description', {}))
+        )
         ARGS_HEADER.update(colors.get('header', {}))
         ARGS_LABEL.update(colors.get('label', {}))
         ARGS_OPTIONS.update(colors.get('options', {}))
