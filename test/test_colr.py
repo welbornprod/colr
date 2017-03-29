welbornprod/colr

commit 39888f8655c205185edecc6bab115b56c02777d4

True color (rgb), with methods and tests.

test/test_colr.py
@@ -31,6 +31,11 @@ from colr import (
     term2rgb,
     strip_codes,
 )
+from colr.trans import (
+    is_code,
+    is_ext_code,
+    is_rgb_code,
+)
 
 # Save names in list format, for random.choice().
 name_data_names = list(name_data)
@@ -92,17 +97,82 @@ class ColrTest(unittest.TestCase):
 
     def test_chained_attr(self):
         """ Colr should allow chained color named methods. """
-        # This will raise an AttributeError on failure.
+        # This will raise an AttributeError if the chained method is
+        # not recognized.
+        try:
+            self.assertIsInstance(
+                Colr().reset().bg_white(),
+                Colr,
+                msg='Failed to create Colr with chained methods.'
+            )
+            self.assertIsInstance(
+                Colr().f_155().b_233(),
+                Colr,
+                msg='Failed to create Colr with chained methods.'
+            )
+        except AttributeError as ex:
+            self.fail('Failed to recognize known chained method: {}'.format(
+                ex
+            ))
+
+        # RGB codes should work.
         self.assertIsInstance(
-            Colr().reset().bg_white(),
+            Colr().rgb(255, 255, 255),
             Colr,
-            msg='Failed to create Colr with chained methods.'
+            msg='Failed to create Colr with chained rgb method.'
         )
+
         self.assertIsInstance(
-            Colr().f_155().b_233(),
+            Colr().b_rgb(255, 255, 255),
             Colr,
-            msg='Failed to create Colr with chained methods.'
+            msg='Failed to create Colr with chained b_rgb method.'
         )
+
+        with self.assertRaises(ValueError):
+            Colr().rgb(256, 0, 0)
+        with self.assertRaises(ValueError):
+            Colr().rgb(-1, 0, 0)
+        # Invalid rgb codes should raise a ValueError.
+        with self.assertRaises(ValueError):
+            Colr().b_rgb(256, 0, 0)
+        with self.assertRaises(ValueError):
+            Colr().b_rgb(256, 0, 0)
+
+    def test_color(self):
+        """ Colr.color should accept valid color names/values. """
+        # None of these should raise a ValueError.
+        s = 'test'
+        try:
+            Colr(s, 'red')
+            Colr(s, 16)
+            Colr(s, (255, 0, 0))
+        except ValueError as ex:
+            self.fail(
+                'ValueError raised for valid color name/value: {}'.format(
+                    ex
+                )
+            )
+
+        # Should get the correct code type for the correct value.
+        self.assertTrue(
+            is_code(str(Colr(' ', 'red')).split()[0])
+        )
+        self.assertTrue(
+            is_ext_code(str(Colr(' ', 56)).split()[0])
+        )
+        self.assertTrue(
+            is_rgb_code(str(Colr(' ', (0, 0, 255))).split()[0])
+        )
+
+        # Should raise ValueError on invalid color name/value.
+        with self.assertRaises(ValueError):
+            Colr(s, 'NOTACOLOR')
+        with self.assertRaises(ValueError):
+            Colr(s, 257)
+        with self.assertRaises(ValueError):
+            Colr(s, (-1, 0, 0))
+        with self.assertRaises(ValueError):
+            Colr(s, (257, 0, 0))
 
     def test_colorcode(self):
         """ ColorCode should properly translate codes. """
@@ -171,13 +241,43 @@ class ColrTest(unittest.TestCase):
             msg=test_msg(
                 'Failed to convert short form hex string.', *argset, **kwset))
 
+    def test_is_code(self):
+        """ colr.trans.is_code should recognize a color code. """
+        validcodes = ('\033[31m', '\033[41m')
+        invalidcode = '\033[38;5;27m'
+        for validcode in validcodes:
+            self.assertTrue(is_code(validcode))
+        self.assertFalse(is_code(invalidcode))
+
+    def test_is_ext_code(self):
+        """ colr.trans.is_ext_code should recognize a color code. """
+        validcodes = ('\033[38;5;42m', '\033[48;5;42m')
+        invalidcode = '\033[10m'
+        for validcode in validcodes:
+            self.assertTrue(is_ext_code(validcode))
+        self.assertFalse(is_ext_code(invalidcode))
+
+    def test_is_rgb_code(self):
+        """ colr.trans.is_rgb_code should recognize a color code. """
+        validcodes = ('\033[38;2;0;0;255m', '\033[48;2;0;0;255m')
+        invalidcode = '\033[10m'
+        for validcode in validcodes:
+            self.assertTrue(is_rgb_code(validcode))
+        self.assertFalse(is_rgb_code(invalidcode))
+
     def test_name_data(self):
         """ Colr should use name_data.names when all other style names fail.
         """
         for _ in range(5):
             knownname = random.choice(name_data_names)
             # If this doesn't raise a ValueError we should be okay.
-            Colr('hello world', fore=knownname)
+            try:
+                Colr('hello world', fore=knownname)
+            except ValueError as ex:
+                self.fail('Raised ValueError on known name: {}\n{}'.format(
+                    knownname,
+                    ex
+                ))
 
     def test_name_data_attr(self):
         """ Colr should recognize fg_<name_data> and bg_<name_data> attrs. """

colr/__main__.py
@@ -12,7 +12,7 @@
 
 import os
 import sys
-
+import traceback
 from random import randint
 
 from .colr import (
@@ -52,24 +52,29 @@ USAGESTR = """{versionstr}
     Usage:
         {script} -h | -v
         {script} [TEXT] [FORE] [BACK] [STYLE]
-                 [-a] [-e] [-c num | -l num | -r num] [-n]
+                 [-a] [-e] [-c num | -l num | -r num] [-n] [-D]
         {script} [TEXT] [-f fore] [-b back] [-s style]
-                 [-a] [-e] [-c num | -l num | -r num] [-n]
-        {script} [TEXT] [FORE] [BACK] [STYLE]
-                 [-a] [-e] [-c num | -l num | -r num] [-n] -g name [-q num] [-w num]
-        {script} [TEXT] [-f fore] [-b back] [-s style ]
-                 [-a] [-e] [-c num | -l num | -r num] [-n] -g name [-q num] [-w num]
-        {script} [TEXT] [FORE] [BACK] [STYLE]
-                 [-a] [-e] [-c num | -l num | -r num] [-n] -R [-o num] [-q num] [-w num]
-        {script} [TEXT] [-f fore] [-b back] [-s style]
-                 [-a] [-e] [-c num | -l num | -r num] [-n] -R [-o num] [-q num] [-w num]
-        {script} -x [TEXT] [-a] [-e] [-c num | -l num | -r num] [-n]
-        {script} -t [-a] [CODE...]
-        {script} -z [-a] [-u] [TEXT]
+                 [-a] [-e] [-c num | -l num | -r num] [-n] [-D]
+        {script} [TEXT] [FORE] [BACK] [STYLE] [-a] [-e]
+                 [-c num | -l num | -r num] [-n] -g name
+                 [-q num] [-w num] [-T] [-D]
+        {script} [TEXT] [-f fore] [-b back] [-s style ] [-a] [-e]
+                 [-c num | -l num | -r num] [-n] -g name
+                 [-q num] [-w num] [-T] [-D]
+        {script} [TEXT] [FORE] [BACK] [STYLE] [-a] [-e]
+                 [-c num | -l num | -r num] [-n] -R [-o num]
+                 [-q num] [-w num] [-T] [-D]
+        {script} [TEXT] [-f fore] [-b back] [-s style] [-a] [-e]
+                 [-c num | -l num | -r num] [-n] -R [-o num]
+                 [-q num] [-w num] [-T] [-D]
+        {script} -x [TEXT] [-a] [-e] [-c num | -l num | -r num] [-n] [-D]
+        {script} -t [-a] [CODE...] [-T] [-D]
+        {script} -z [-a] [-T] [-u] [TEXT] [-D]
 
     Options:
         CODE                      : One or more codes to translate.
-        TEXT                      : Text to print. If not given, stdin is used.
+        TEXT                      : Text to print.
+                                    Default: stdin
         FORE                      : Name or number for fore color to use.
         BACK                      : Name or number for back color to use.
         STYLE                     : Name for style to use.
@@ -78,6 +83,8 @@ USAGESTR = """{versionstr}
         -b name,--back name       : Name or number for back color to use.
         -c num,--center num       : Center justify the text before coloring,
                                     using `num` as the overall width.
+        -D,--debug                : Debug mode, print more information while
+                                    running, or on errors.
         -e,--err                  : Print to stderr instead of stdout.
         -f name,--fore name       : Name or number for fore color to use.
         -g name,--gradient name   : Use the gradient method by color name.
@@ -99,6 +106,7 @@ USAGESTR = """{versionstr}
         -s name,--style name      : Name for style to use.
         -t,--translate            : Translate one or more term codes,
                                     hex values, or rgb values.
+        -T,--truecolor            : Use RGB mode when applicable.
         -u,--unique               : Only report unique color codes with -z.
         -w num,--spread num       : Spread/width of each color in the rainbow
                                     or gradient.
@@ -112,17 +120,28 @@ USAGESTR = """{versionstr}
 
 """.format(script=SCRIPT, versionstr=VERSIONSTR)  # noqa
 
+DEBUG = False
+
 
 def main():
     """ Main entry point, expects doctopt arg dict as argd. """
+    global DEBUG
+
     argd = docopt(USAGESTR, version=VERSIONSTR, script=SCRIPT)
+    DEBUG = argd['--debug']
+
     if argd['--auto-disable']:
         auto_disable()
 
     if argd['--translate']:
         # Just translate a simple code and exit.
         try:
-            print('\n'.join(translate(argd['CODE'] or read_stdin().split())))
+            print('\n'.join(
+                translate(
+                    argd['CODE'] or read_stdin().split(),
+                    rgb_mode=argd['--truecolor'],
+                )
+            ))
         except ValueError as ex:
             print_err('Translation error: {}'.format(ex))
             return 1
@@ -131,7 +150,8 @@ def main():
         # List all escape codes found in some text and exit.
         return list_known_codes(
             argd['TEXT'] or read_stdin(),
-            unique=argd['--unique']
+            unique=argd['--unique'],
+            rgb_mode=argd['--truecolor'],
         )
 
     txt = argd['TEXT'] or read_stdin()
@@ -168,7 +188,8 @@ def get_colr(txt, argd):
                 spread=try_int(argd['--spread'], 1, minimum=0),
                 fore=fore,
                 back=back,
-                style=style
+                style=style,
+                rgb_mode=argd['--truecolor'],
             )
         except ValueError as ex:
             print_err('Error: {}'.format(ex))
@@ -180,7 +201,8 @@ def get_colr(txt, argd):
             style=style,
             freq=try_float(argd['--frequency'], 0.1, minimum=0),
             offset=try_int(argd['--offset'], randint(0, 255), minimum=0),
-            spread=try_float(argd['--spread'], 3.0, minimum=0)
+            spread=try_float(argd['--spread'], 3.0, minimum=0),
+            rgb_mode=argd['--truecolor'],
         )
 
     else:
@@ -212,12 +234,12 @@ def justify(clr, argd):
     return clr
 
 
-def list_known_codes(s, unique=True):
+def list_known_codes(s, unique=True, rgb_mode=False):
     """ Find and print all known escape codes in a string,
         using get_known_codes.
     """
     total = 0
-    for codedesc in get_known_codes(s, unique=unique):
+    for codedesc in get_known_codes(s, unique=unique, rgb_mode=rgb_mode):
         total += 1
         print(codedesc)
     plural = 'code' if total == 1 else 'codes'
@@ -241,7 +263,7 @@ def read_stdin():
     return sys.stdin.read()
 
 
-def translate(usercodes):
+def translate(usercodes, rgb_mode=False):
     """ Translate one or more hex, term, or rgb value into the others.
         Yields strings with the results for each code translated.
     """
@@ -258,7 +280,7 @@ def translate(usercodes):
                     raise InvalidNumber(code, label='Invalid rgb value:')
                 code = (r, g, b)
 
-            colorcode = ColorCode(code)
+            colorcode = ColorCode(code, rgb_mode=rgb_mode)
 
             if disabled():
                 yield str(colorcode)
@@ -334,7 +356,10 @@ if __name__ == '__main__':
         print_err('\nBroken pipe, input/output was interrupted.\n')
         mainret = 3
     except (ValueError, InvalidNumber) as exnum:
-        print_err('\n{}'.format(exnum))
+        if DEBUG:
+            print_err(traceback.format_exc())
+        else:
+            print_err('\n{}'.format(exnum))
         mainret = 4
 
     sys.exit(mainret)

colr/trans.py
@@ -19,7 +19,7 @@ Resources:
 
 
 """
-
+import re
 from types import GeneratorType
 from typing import cast, Any, Optional, Sequence, TypeVar
 
@@ -308,7 +308,7 @@ def fix_hex(hexval: str) -> str:
         hexval = '{r}{r}{g}{g}{b}{b}'.format(**rgbvals)
     elif hexlen != 6:
         raise ValueError(
-            'Expecting a hex string (#RGB, #RRGGBB), got: {}'.format(
+            'Not a length 3 or 6 hex string (#RGB, #RRGGBB), got: {}'.format(
                 hexval))
     return hexval
 
@@ -317,15 +317,19 @@ def hex2rgb(hexval: str, allow_short: bool=False) -> Sequence[int]:
     """ Return a tuple of (R, G, B) from a hex color. """
     if not hexval:
         raise ValueError(
-            'Expecting a hex string (#RGB, #RRGGBB), got: {}'.format(
-                hexval))
+            'Expecting hex string (#RGB, #RRGGBB), got nothing: {!r}'.format(
+                hexval
+            )
+        )
     hexval = hexval.strip().lstrip('#')
     if allow_short:
         hexval = fix_hex(hexval)
     if not len(hexval) == 6:
         raise ValueError(
-            'Expecting a hex string (#RGB, #RRGGBB), got: {}'.format(
-                hexval))
+            'Not a length 3 or 6 hex string (#RGB, #RRGGBB), got: {}'.format(
+                hexval
+            )
+        )
     try:
         val = tuple(
             int(''.join(hexval[i:i + 2]), 16)
@@ -345,6 +349,23 @@ def hex2term(hexval: str, allow_short: bool=False) -> str:
 def hex2termhex(hexval: str, allow_short: bool=False) -> str:
     """ Convert a hex value into the nearest terminal color matched hex. """
     return rgb2termhex(*hex2rgb(hexval, allow_short=allow_short))
+
+
+def is_code(s: str) -> bool:
+    """ Returns True if `s` appears to be a single basic escape code. """
+    return re.match('^\033\\[\\d{1,2}m$', s) is not None
+
+
+def is_ext_code(s: str) -> bool:
+    """ Returns True if `s` appears to be a single extended 256 escape code.
+    """
+    return re.match('^\033\\[((38)|(48));5;\\d{1,3}m$', s) is not None
+
+
+def is_rgb_code(s: str) -> bool:
+    """ Returns True if `s` appears to be a single rgb escape code. """
+    pattern = '^\033\\[((38)|(48));2;\\d{1,3};\\d{1,3};\\d{1,3}m'
+    return re.match(pattern, s) is not None
 
 
 def print_all() -> None:
@@ -373,7 +394,12 @@ def rgb2termhex(r: int, g: int, b: int) -> str:
     incs = [0x00, 0x5f, 0x87, 0xaf, 0xd7, 0xff]
 
     res = []
-    for part in (r, g, b):
+    parts = r, g, b
+    for part in parts:
+        if (part < 0) or (part > 255):
+            raise ValueError(
+                'Expecting 0-255 for RGB code, got: {!r}'.format(parts)
+            )
         i = 0
         while i < len(incs) - 1:
             s, b = incs[i], incs[i + 1]  # smaller, bigger
@@ -421,12 +447,16 @@ class ColorCode(object):
             hexval : Nearest matching hex value.
             rgb    : Tuple of nearest matching (Red, Green, Blue) values.
     """
-    __slots__ = ['code', 'hexval', 'rgb']
+    __slots__ = ('code', 'hexval', 'rgb', 'rgb_mode')
 
-    def __init__(self, code: Any=None) -> None:
+    def __init__(
+            self,
+            code: Optional[Any]=None,
+            rgb_mode: Optional[bool]=False) -> None:
         self.rgb = tuple()  # type: Sequence[int]
         self.hexval = None  # type: str
         self.code = None  # type: str
+        self.rgb_mode = rgb_mode  # type: bool
         # Init tries to be smart about converting code types.
         typeerrmsg = 'Expecting hex, term-code, or rgb. Got: {}'.format(
             getattr(code, '__name__', type(code).__name__)
@@ -491,14 +521,22 @@ class ColorCode(object):
 
     def _init_rgb(self, r: int, g: int, b: int) -> None:
         """ Initialize from red, green, blue args. """
-        self.rgb = hex2rgb(rgb2termhex(r, g, b))
-        self.hexval = rgb2termhex(r, g, b)
+        if self.rgb_mode:
+            self.rgb = (r, g, b)
+            self.hexval = rgb2hex(r, g, b)
+        else:
+            self.rgb = hex2rgb(rgb2termhex(r, g, b))
+            self.hexval = rgb2termhex(r, g, b)
+
         self.code = hex2term(self.hexval)
 
     def example(self) -> str:
         """ Same as str(self), except the color codes are actually used. """
-
-        return '\033[38;5;{s.code}m{s}\033[0m'.format(s=self)
+        if self.rgb_mode:
+            colorcode = '\033[38;2;{};{};{}m'.format(*self.rgb)
+        else:
+            colorcode = '\033[38;5;{}m'.format(self.code)
+        return '{code}{s}\033[0m'.format(code=colorcode, s=self)
 
     @classmethod
     def from_code(cls, code: int) -> 'ColorCode':

commit 73718b8fefcb60f08a387dc0e996b83654c94584

Fix `gradient` regression introduced in `39888f86`.
`offset` would be used before initialization in most cases.

duh.

colr/colr.py
@@ -1650,7 +1650,7 @@ class Colr(object):
                 )
             try:
                 # Get rainbow offset from known name.
-                self.gradient_names[name]
+                offset = self.gradient_names[name]
             except KeyError:
                 raise ValueError('Unknown gradient name: {}'.format(name))
 

test/test_colr.py
@@ -386,6 +386,33 @@ class ColrTests(ColrTestCase):
             msg='Colr(\'{}\').format(Colr()) breaks formatting!',
         )
 
+    def test_gradient(self):
+        """ Colr.gradient should recognize names and rainbow offsets. """
+        valid_names = list(Colr.gradient_names)
+        valid_names.extend(range(1, 254))
+        valid_names.extend((False, True, None, '', 0, 1.2))
+        for valid_name in valid_names:
+            try:
+                Colr('test').gradient(name=valid_name)
+            except ValueError:
+                self.fail(self.call_msg(
+                    'Colr.gradient failed on a known name.',
+                    valid_name,
+                    func=Colr().gradient,
+                ))
+        invalid_names = ('block', 'bash', 'berry')
+        for invalid_name in invalid_names:
+            try:
+                Colr('test').gradient(name=invalid_name)
+            except ValueError:
+                pass
+            else:
+                self.fail(self.call_msg(
+                    'Failed to raise on invalid name.',
+                    invalid_name,
+                    func=Colr().gradient,
+                ))
+
     def test_hash(self):
         """ hash(Colr()) should return a unique hash for self.data. """
         a, b = hash(Colr('test', 'red')), hash(Colr('test', 'red'))